68f5e6f08cb599c6935b11e3a677c917300d7999
Cindy <73166664+cindy0627@users.noreply.github.com>
2023-03-22 08:07:06 +0800

    Update stroke_model.py

--- stroke_model.py
+++ stroke_model.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import seaborn as sns
 import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib as matplot
 import sklearn
 
@@ -15,25 +14,27 @@ y = df_final.MCQ160F
 
 from sklearn.preprocessing import MinMaxScaler
 import joblib
-from joblib import Parallel, delayed
+
 minmax = MinMaxScaler()
 X = pd.DataFrame(minmax.fit_transform(X_), columns=X_.columns)
 joblib.dump(minmax, 'minmax_stroke.gz')
 
-
 # oversample
 from sklearn.model_selection import train_test_split
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1)  # 0.25 x 0.8 = 0.2
 
 from imblearn.over_sampling import SMOTE
+
 smote = SMOTE(random_state=0)
 X_train_sm, y_train_sm = smote.fit_resample(X_train, y_train)
 X_train_sm = pd.DataFrame(X_train_sm, columns=X.columns)
 
 from sklearn.ensemble import RandomForestClassifier
-best_random = RandomForestClassifier(n_estimators =100,max_depth=5,bootstrap=True, random_state=42)
+
+best_random = RandomForestClassifier(n_estimators=100, max_depth=5, bootstrap=True, random_state=42)
 best_random.fit(X_train_sm, y_train_sm)
 
-# save
-joblib.dump(best_random, 'rnd_clf.pkl')
+# save the model
+joblib.dump(best_random, 'rnd_clf_stroke.pkl')
