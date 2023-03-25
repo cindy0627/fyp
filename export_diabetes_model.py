679ea24a2265e792888be0218c198916d5186669
Cindy <73166664+cindy0627@users.noreply.github.com>
2023-03-26 07:55:24 +0800

    Update export_diabetes_model.py

--- export_diabetes_model.py
+++ export_diabetes_model.py
@@ -1,9 +1,4 @@
 import pandas as pd
-import seaborn as sns
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib as matplot
-import sklearn
 import warnings
 
 warnings.filterwarnings("ignore")
@@ -14,7 +9,6 @@ y = df_final.DIQ010
 
 from sklearn.preprocessing import MinMaxScaler
 import joblib
-from joblib import Parallel, delayed
 
 minmax = MinMaxScaler()
 X = pd.DataFrame(minmax.fit_transform(X_), columns=X_.columns)
@@ -22,23 +16,21 @@ joblib.dump(minmax, 'minmax_dia.gz')
 
 # oversample
 from sklearn.model_selection import train_test_split
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1)  # 0.25 x 0.8 = 0.2
 
 from imblearn.over_sampling import SMOTE
+
 smote = SMOTE(random_state=0)
 X_train_sm, y_train_sm = smote.fit_resample(X_train, y_train)
 X_train_sm = pd.DataFrame(X_train_sm, columns=X.columns)
 
-
 from sklearn.linear_model import LogisticRegression
-best_random = LogisticRegression(C= 7.853887219515014, max_iter= 100, solver='liblinear', random_state=11)
+
+best_random = LogisticRegression(C=7.853887219515014, max_iter=100, solver='liblinear', random_state=11)
 best_random.fit(X_train_sm, y_train_sm)
 y_pred_lr2 = best_random.predict(X_test)
-
-
-print('Accuracy Score:', best_random.score(X_test, y_test))
-print('Prediction:', y_pred_lr2)
 
 # save
 joblib.dump(best_random, 'lr_clf_dia.pkl')
